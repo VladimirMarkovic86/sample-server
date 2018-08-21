ab19ec2025da6e068be68d682924983dcb4ee347
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-08-21 19:33:00 +0200

    Reformatting project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject sample-server "0.1.0"
+(defproject org.vladimir/sample-server "0.1.0"
   :description "Sample server"
   :url "http://gitlab:1610/VladimirMarkovic86/sample-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure			   "1.9.0"]
-         									;	https://clojure.org/api/api
+  :dependencies [[org.clojure/clojure	"1.9.0"]
          								[org.vladimir/server-lib "0.1.0"]
          								[org.vladimir/mongo-lib "0.1.0"]
          								[org.vladimir/utils-lib "0.1.0"]
@@ -16,4 +15,5 @@
   ; AOT - Compailation ahead of time
   :main ^:skip-aot sample-server.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :repl-options {:port 8603})
