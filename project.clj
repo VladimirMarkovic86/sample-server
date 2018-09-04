6d7fa6d747973a485ba9e37e721d906e5e7acec1
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-09-04 18:14:07 +0200

    Adjusting code to common libraries.

--- project.clj
+++ project.clj
@@ -6,11 +6,9 @@
   :dependencies [[org.clojure/clojure	"1.9.0"]
          								[org.vladimir/server-lib "0.1.0"]
          								[org.vladimir/mongo-lib "0.1.0"]
-         								[org.vladimir/utils-lib "0.1.0"]
          								[org.vladimir/ajax-lib "0.1.0"]
          								[org.vladimir/session-lib "0.1.0"]
-         								[org.vladimir/dao-lib "0.1.0"]
-         								[org.vladimir/language-lib "0.1.0"]
+         								[org.vladimir/common-server "0.1.0"]
          								]
   
   ; AOT - Compailation ahead of time
--- src/sample_server/core.clj
+++ src/sample_server/core.clj
@@ -1,82 +1,20 @@
 (ns sample-server.core
   (:require [session-lib.core :as ssn]
             [server-lib.core :as srvr]
-            [utils-lib.core :as utils]
             [mongo-lib.core :as mon]
-            [dao-lib.core :as dao]
-            [language-lib.core :as lang]
-            [ajax-lib.http.entity-header :as eh]
-            [ajax-lib.http.response-header :as rsh]
-            [ajax-lib.http.mime-type :as mt]
-            [ajax-lib.http.status-code :as stc]))
+            [common-server.core :as rt]
+            [ajax-lib.http.response-header :as rsh]))
 
 (def db-name
      "sample-db")
 
-(defn not-found
-  "Requested action not found"
-  []
-  {:status (stc/not-found)
-   :headers {(eh/content-type) (mt/text-plain)}
-   :body (str {:status "error"
-               :error-message "404 not found"})})
-
-(defn parse-body
-  "Read entity-body from request, convert from string to clojure data"
-  [request]
-  (read-string
-    (:body request))
- )
-
 (defn routing
-  "Routing function"
+  ""
   [request-start-line
    request]
-  (println
-    (str
-      "\n"
-      (dissoc
-        request
-        :body))
-   )
-  (if (ssn/am-i-logged-in-fn request)
-    (let [[cookie-key
-           cookie-value] (ssn/refresh-session
-                           request)
-          response
-           (case request-start-line
-             "POST /am-i-logged-in" (ssn/am-i-logged-in request)
-             "POST /get-entities" (dao/get-entities (parse-body request))
-             "POST /get-entity" (dao/get-entity (parse-body request))
-             "POST /update-entity" (dao/update-entity (parse-body request))
-             "POST /insert-entity" (dao/insert-entity (parse-body request))
-             "DELETE /delete-entity" (dao/delete-entity (parse-body request))
-             "POST /logout" (ssn/logout request)
-             "POST /get-labels" (lang/get-labels request)
-             "POST /set-language" (lang/set-language
-                                    request
-                                    (parse-body request))
-             {:status (stc/not-found)
-              :headers {(eh/content-type) (mt/text-plain)}
-              :body (str {:status  "success"})})]
-      (update-in
-        response
-        [:headers]
-        assoc
-        cookie-key
-        cookie-value))
-    (case request-start-line
-      "POST /login" (ssn/login-authentication
-                      (parse-body
-                        request)
-                      (:user-agent request))
-      "POST /sign-up" (dao/insert-entity (parse-body request))
-      "POST /am-i-logged-in" (ssn/am-i-logged-in request)
-      "POST /get-labels" (lang/get-labels request)
-      {:status (stc/unauthorized)
-       :headers {(eh/content-type) (mt/text-plain)}
-       :body (str {:status  "success"})})
-   ))
+  (rt/routing
+    request-start-line
+    request))
 
 (defn start-server
   "Start server"
