2f94ba27561660fe34bfb86c881bd8b0d5186a6f
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-25 13:36:35 +0200

    Prepared sample-server for taking part in sample-app.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.0"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.1"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.0"]
                  [org.clojars.vladimirmarkovic86/common-server "0.2.0"]
@@ -14,6 +14,8 @@
 
   :min-lein-version "2.0.0"
 
+  :source-paths ["src/clj"]
+  
   :main ^:skip-aot sample-server.core
   
   :uberjar-name "sample-server-standalone.jar"
