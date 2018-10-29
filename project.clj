2d6497c356d10ffb27f0c885c6e2b4340d37faa0
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-29 12:29:10 +0100

    Heroku

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.3"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.1"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.1"]
                  [org.clojars.vladimirmarkovic86/common-server "0.3.0"]
