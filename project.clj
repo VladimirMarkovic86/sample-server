797dc9a186ebae2e674d0b1c3753b1cf0d936591
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-11-01 13:07:16 +0100

    Implemented audit functionality.

--- project.clj
+++ project.clj
@@ -4,12 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.7"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.8"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.1"]
                  [org.clojars.vladimirmarkovic86/common-server "0.3.2"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.3.0"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.0"]
+                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.0"]
                  ]
 
   :min-lein-version "2.0.0"
