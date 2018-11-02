4235da4f5ddee5e9a73bd3ccd07e112326a6ff19
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-11-02 17:02:41 +0100

    Changed version and implemented database update mechanism.

--- src/clj/sample_server/scripts.clj
+++ src/clj/sample_server/scripts.clj
@@ -1,12 +1,28 @@
 (ns sample-server.scripts
   (:require [mongo-lib.core :as mon]
-            [utils-lib.core :as utils]))
+            [utils-lib.core :as utils]
+            [common-middle.collection-names :refer [db-updates-cname
+                                                    language-cname
+                                                    role-cname
+                                                    user-cname
+                                                    preferences-cname]]
+            [common-middle.role-names :refer [user-admin-rname
+                                              user-mod-rname
+                                              language-admin-rname
+                                              language-mod-rname
+                                              role-admin-rname
+                                              role-mod-rname
+                                              test-privileges-rname]]
+            [sample-middle.role-names :refer [person-admin-rname
+                                              person-mod-rname]]
+            [common-middle.functionalities :as fns]
+            [sample-middle.functionalities :as smfns]))
 
 (defn initialize-db
   "Initialize database"
   []
   (mon/mongodb-insert-many
-    "language"
+    language-cname
     [{:code 1 :english "Save" :serbian "Сачувај" }
      {:code 2, :english "Log out", :serbian "Одјави се" }
      {:code 3, :english "Home", :serbian "Почетна" }
@@ -46,55 +62,68 @@
      {:code 1007, :english "Gender", :serbian "Пол" }
      {:code 1008, :english "Diet", :serbian "Исхрана" }
      {:code 1009, :english "Activity", :serbian "Активност" }
-     {:code 1010, :english "Test person entity", :serbian "Тестирај ентитет особа" }
      {:code 31, :english "No entities", :serbian "Нема ентитета" }])
   (mon/mongodb-insert-many
-    "role"
-    [{:role-name "User administrator",
-      :functionalities [ "user-create", "user-read", "user-update", "user-delete" ] }
-     {:role-name "Person administrator",
-      :functionalities [ "person-create", "person-read", "person-update", "person-delete" ] }
-     {:role-name "Language administrator",
-      :functionalities [ "language-create", "language-read", "language-update", "language-delete" ] }
-     {:role-name "User moderator",
-      :functionalities [ "user-read", "user-update" ] }
-     {:role-name "Person moderator",
-      :functionalities [ "person-read", "person-update" ] }
-     {:role-name "Language moderator",
-      :functionalities [ "language-read", "language-update" ] }
-     {:role-name "Role administrator",
-      :functionalities [ "role-create", "role-read", "role-update", "role-delete" ] }
-     {:role-name "Role moderator",
-      :functionalities [ "role-read", "role-update" ] }
-     {:role-name "Role moderator 2",
-      :functionalities [ "role-read" ]}
-     {:role-name "Test privileges",
-      :functionalities [ "test-person-entity" ]}])
+    role-cname
+    [{:role-name user-admin-rname
+      :functionalities [fns/user-create
+                        fns/user-read
+                        fns/user-update
+                        fns/user-delete]}
+     {:role-name user-mod-rname
+      :functionalities [fns/user-read
+                        fns/user-update]}
+     {:role-name language-admin-rname
+      :functionalities [fns/language-create
+                        fns/language-read
+                        fns/language-update
+                        fns/language-delete]}
+     {:role-name language-mod-rname
+      :functionalities [fns/language-read
+                        fns/language-update]}
+     {:role-name role-admin-rname
+      :functionalities [fns/role-create
+                        fns/role-read
+                        fns/role-update
+                        fns/role-delete]}
+     {:role-name role-mod-rname
+      :functionalities [fns/role-read
+                        fns/role-update]}
+     {:role-name person-admin-rname
+      :functionalities [smfns/person-create
+                        smfns/person-read
+                        smfns/person-update
+                        smfns/person-delete]}
+     {:role-name person-mod-rname
+      :functionalities [smfns/person-read
+                        smfns/person-update]}
+     {:role-name test-privileges-rname
+      :functionalities [smfns/test-person-entity]}])
   (let [user-admin-id (:_id
                         (mon/mongodb-find-one
-                          "role"
-                          {:role-name "User administrator"}))
-        person-admin-id (:_id
-                          (mon/mongodb-find-one
-                            "role"
-                            {:role-name "Person administrator"}))
+                          role-cname
+                          {:role-name user-admin-rname}))
         language-admin-id (:_id
                             (mon/mongodb-find-one
-                              "role"
-                              {:role-name "Language administrator"}))
+                              role-cname
+                              {:role-name language-admin-rname}))
         role-admin-id (:_id
                         (mon/mongodb-find-one
-                          "role"
-                          {:role-name "Role administrator"}))
+                          role-cname
+                          {:role-name role-admin-rname}))
+        person-admin-id (:_id
+                          (mon/mongodb-find-one
+                            role-cname
+                            {:role-name person-admin-rname}))
         test-admin-id (:_id
                         (mon/mongodb-find-one
-                          "role"
-                          {:role-name "Test privileges"}))
+                          role-cname
+                          {:role-name test-privileges-rname}))
         encrypted-password (utils/encrypt-password
                              (or (System/getenv "ADMIN_USER_PASSWORD")
                                  "123"))]
     (mon/mongodb-insert-one
-      "user"
+      user-cname
       {:username "admin"
        :email "123@123"
        :password encrypted-password
@@ -105,11 +134,29 @@
                test-admin-id]}))
   (let [user-id (:_id
                   (mon/mongodb-find-one
-                    "user"
+                    user-cname
                     {}))]
     (mon/mongodb-insert-one
-      "preferences"
-      {:user-id user-id, :language "serbian", :language-name "Srpski" }))
+      preferences-cname
+      {:user-id user-id
+       :language "serbian"
+       :language-name "Srpski" }))
+  (mon/mongodb-insert-one
+    db-updates-cname
+    {:initialized true
+     :date (java.util.Date.)})
+ )
+
+(defn db-update-1
+  "Database update 1"
+  []
+  (mon/mongodb-insert-many
+    language-cname
+    [{:code 1010, :english "Test person entity", :serbian "Тестирај ентитет особа" }])
+  (mon/mongodb-insert-one
+    db-updates-cname
+    {:update 1
+     :date (java.util.Date.)})
  )
 
 (defn initialize-db-if-needed
@@ -117,9 +164,13 @@
   []
   (try
     (when-not (mon/mongodb-exists
-                "language"
-                {:english "Save"})
+                db-updates-cname
+                {:initialized true})
       (initialize-db))
+    (when-not (mon/mongodb-exists
+                db-updates-cname
+                {:update 1})
+      (db-update-1))
     (catch Exception e
       (println e))
    ))
