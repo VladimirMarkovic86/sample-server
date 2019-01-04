2613976ba75952ea290285a99d9d360a8d886d08
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-01-04 12:40:24 +0100

    Task: 7 Design improved for ocr, po and sample projects.

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.12"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.1"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.4"]
-                 [org.clojars.vladimirmarkovic86/common-server "0.3.8"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.13"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.2"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.5"]
+                 [org.clojars.vladimirmarkovic86/common-server "0.3.9"]
                  [org.clojars.vladimirmarkovic86/sample-middle "0.3.2"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.2"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.2"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.1"]
-                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.6"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.3"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.2"]
+                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.7"]
                  ]
 
   :min-lein-version "2.0.0"
--- src/clj/sample_server/scripts.clj
+++ src/clj/sample_server/scripts.clj
@@ -53,6 +53,7 @@
      {:code 28, :english "Role name", :serbian "Назив улоге" }
      {:code 29, :english "Functionalities", :serbian "Функционалности" }
      {:code 30, :english "Roles", :serbian "Улоге" }
+     {:code 31, :english "No entities", :serbian "Нема ентитета" }
      {:code 1001, :english "Person", :serbian "Особа" }
      {:code 1002, :english "First name", :serbian "Име" }
      {:code 1003, :english "Last name", :serbian "Презиме" }
@@ -61,8 +62,7 @@
      {:code 1006, :english "Birthday", :serbian "Датум рођења" }
      {:code 1007, :english "Gender", :serbian "Пол" }
      {:code 1008, :english "Diet", :serbian "Исхрана" }
-     {:code 1009, :english "Activity", :serbian "Активност" }
-     {:code 31, :english "No entities", :serbian "Нема ентитета" }])
+     {:code 1009, :english "Activity", :serbian "Активност" }])
   (mon/mongodb-insert-many
     role-cname
     [{:role-name user-admin-rname
@@ -172,7 +172,75 @@
      {:code 1016, :english "All", :serbian "Све" }
      {:code 1017, :english "Vegetarian", :serbian "Вегетаријанска" }
      {:code 1018, :english "Male", :serbian "Мушки" }
-     {:code 1019, :english "Female", :serbian "Женски" }])
+     {:code 1019, :english "Female", :serbian "Женски" }
+     {:code 32, :english "Administration", :serbian "Администрација" }
+     {:code 33, :english "- Select -", :serbian "- Одабери -" }
+     {:code 34, :english "first", :serbian "прва" }
+     {:code 35, :english "previous", :serbian "претходна" }
+     {:code 36, :english "next", :serbian "следећа" }
+     {:code 37, :english "last", :serbian "задња" }
+     {:code 38, :english "Bad input", :serbian "Погрешан унос" }
+     {:code 39, :english "Custom error", :serbian "Грешка" }
+     {:code 40, :english "Pattern mismatch", :serbian "Не поклапање шаблона" }
+     {:code 41, :english "Range overflow", :serbian "Прекорачење опсега" }
+     {:code 42, :english "Range underflow", :serbian "Не достигнут опсег" }
+     {:code 43, :english "Step mismatch", :serbian "Не поклапање корака" }
+     {:code 44, :english "Too long", :serbian "Предуго" }
+     {:code 45, :english "Too short", :serbian "Прекратко" }
+     {:code 46, :english "Type mismatch", :serbian "Не поклапање типа" }
+     {:code 47, :english "Value missing", :serbian "Недостатак вредности" }
+     {:code 48,
+      :english "Please fill out this field.",
+      :serbian "Молим попуните ово поље." }
+     {:code 49,
+      :english "Please enter a number.",
+      :serbian "Молим унесите број." }
+     {:code 50,
+      :english "Please select one of these options.",
+      :serbian "Молим одаберите једну од ових опција." }
+     {:code 51,
+      :english "Please select an item in the list.",
+      :serbian "Молим одаберите ставку у листи." }
+     {:code 52,
+      :english "Please enter an email address.",
+      :serbian "Молим унесите адресу е-поште." }
+     {:code 53,
+      :english "Please use at least $0 characters (you are currently using $1 characters).",
+      :serbian "Молим искористите бар $0 карактера (тренутно користите $1 карактера)." }
+     {:code 54,
+      :english "Please select a valid value. The two nearest valid values are $0 and $1.",
+      :serbian "Молим одаберите исправну вредност. Две најближе исправне вредности су $0 и $1." }
+     {:code 55,
+      :english "Please select a value that is no less than $0.",
+      :serbian "Молим одаберите вредност која није мања од $0." }
+     {:code 56,
+      :english "Please select a value that is no more than $0.",
+      :serbian "Молим одаберите вредност која није већа од $0." }
+     {:code 57,
+      :english "Please match the requested format.",
+      :serbian "Молим држите се захтеваног формата." }
+     {:code 58,
+      :english "Given email doesn't exist.",
+      :serbian "Прослеђена адреса е-поште не постоји." }
+     {:code 59,
+      :english "Incorrect password for given email.",
+      :serbian "Нетачна лозинка за прослеђену адресу е-поште." }
+     {:code 60,
+      :english "Confirm password does not match with password.",
+      :serbian "Потврдна лозинка се не поклапа са лозинком." }
+     {:code 61,
+      :english "Username and/or email already exists.",
+      :serbian "Корисничко име и/или адреса е-поште већ постоји." }
+     {:code 62,
+      :english "Sample App",
+      :serbian "Sample App" }
+     {:code 63,
+      :english "Sample App is prototype project which can be used to quickly get head start in building your Web Apps.",
+      :serbian "Sample App је прототип пројекат који може да се искористи за брзи почетак у изградњи ваше веб апликације." }
+     {:code 64,
+      :english "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character @$!%*?&..",
+      :serbian "Лозинка мора да садржи барем једно велико слово, једно мало слово, један број и један специјални карактер @$!%*?&.." }
+		   ])
   (mon/mongodb-insert-one
     db-updates-cname
     {:update 2
