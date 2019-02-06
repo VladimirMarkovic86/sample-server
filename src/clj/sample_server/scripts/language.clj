c77aba6322f01941902d5b6d39786572bc04da23
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-02-06 16:28:28 +0100

    Task 38: Improved database scripts during this task.

---
+++ src/clj/sample_server/scripts/language.clj
@@ -0,0 +1,30 @@
+(ns sample-server.scripts.language
+  (:require [mongo-lib.core :as mon]
+            [common-middle.collection-names :refer [language-cname]]))
+
+(defn insert-labels
+  "Inserts labels"
+  []
+  (mon/mongodb-insert-many
+    language-cname
+    [{:code 1001, :english "Person", :serbian "Особа" }
+     {:code 1002, :english "First name", :serbian "Име" }
+     {:code 1003, :english "Last name", :serbian "Презиме" }
+     {:code 1004, :english "Height", :serbian "Висина" }
+     {:code 1005, :english "Weight", :serbian "Тежина" }
+     {:code 1006, :english "Birthday", :serbian "Датум рођења" }
+     {:code 1007, :english "Gender", :serbian "Пол" }
+     {:code 1008, :english "Diet", :serbian "Исхрана" }
+     {:code 1009, :english "Activity", :serbian "Активност" }
+     {:code 1010, :english "Test person entity", :serbian "Тестирај ентитет особа" }
+     {:code 1011, :english "Mainly sitting", :serbian "Углавном седење" }
+     {:code 1012, :english "Easy physical labor", :serbian "Лак физички рад" }
+     {:code 1013, :english "Medium physical labor", :serbian "Средње тежак физички рад" }
+     {:code 1014, :english "Hard physical labor", :serbian "Тежак физички рад" }
+     {:code 1015, :english "Very hard physical labor", :serbian "Веома тежак физички рад" }
+     {:code 1016, :english "All", :serbian "Све" }
+     {:code 1017, :english "Vegetarian", :serbian "Вегетаријанска" }
+     {:code 1018, :english "Male", :serbian "Мушки" }
+     {:code 1019, :english "Female", :serbian "Женски" }
+     ]))
+
---
+++ src/clj/sample_server/scripts/role.clj
@@ -0,0 +1,24 @@
+(ns sample-server.scripts.role
+  (:require [mongo-lib.core :as mon]
+            [common-middle.collection-names :refer [role-cname]]
+            [common-middle.role-names :refer [test-privileges-rname]]
+            [sample-middle.functionalities :as smfns]
+            [sample-middle.role-names :refer [person-admin-rname
+                                              person-mod-rname]]))
+
+(defn insert-roles
+  "Inserts roles"
+  []
+  (mon/mongodb-insert-many
+    role-cname
+    [{:role-name person-admin-rname
+      :functionalities [smfns/person-create
+                        smfns/person-read
+                        smfns/person-update
+                        smfns/person-delete]}
+     {:role-name person-mod-rname
+      :functionalities [smfns/person-read
+                        smfns/person-update]}
+     {:role-name test-privileges-rname
+      :functionalities [smfns/test-person-entity]}]))
+
