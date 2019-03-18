44a15cbb751089b4087781d5753353c6169773c3
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-18 16:09:04 +0100

    Task63: Fixed optimization advanced to work with new code, compiling production version fixed

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.21"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.7"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.11"]
-                 [org.clojars.vladimirmarkovic86/common-server "0.3.19"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.22"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.8"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.12"]
+                 [org.clojars.vladimirmarkovic86/common-server "0.3.20"]
                  [org.clojars.vladimirmarkovic86/sample-middle "0.3.6"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.4"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.6"]
-                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.15"]
+                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.16"]
                  ]
 
   :min-lein-version "2.0.0"
