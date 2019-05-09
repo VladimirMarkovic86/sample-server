19759f0f05754849e106a36fcae5d8e2662f6ac3
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-05-09 17:29:00 +0200

    Task 66: Implemented charts in svg-lib and used it in sample-client.

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.25"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.26"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.9"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.18"]
-                 [org.clojars.vladimirmarkovic86/common-server "0.3.29"]
+                 [org.clojars.vladimirmarkovic86/common-server "0.3.30"]
                  [org.clojars.vladimirmarkovic86/sample-middle "0.3.7"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.6"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.8"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.7"]
-                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.24"]
-                 [org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.1"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.8"]
+                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.25"]
+                 [org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.2"]
                  ]
 
   :min-lein-version "2.0.0"
