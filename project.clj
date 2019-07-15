3c705c62c493c4b62fb685ad3ef54fea44dcec42
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 12:49:51 +0200

    Task 88: Removed public folder from tested clojurescript projects packed jar file.

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.32"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.11"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.25"]
-                 [org.clojars.vladimirmarkovic86/common-server "0.3.41"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.33"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.12"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.26"]
+                 [org.clojars.vladimirmarkovic86/common-server "0.3.42"]
                  [org.clojars.vladimirmarkovic86/sample-middle "0.3.11"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.9"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.10"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.10"]
-                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.33"]
-                 [org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.5"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.11"]
+                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.34"]
+                 [org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.6"]
                  ]
 
   :min-lein-version "2.0.0"
