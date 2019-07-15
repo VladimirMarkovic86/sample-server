60110aa193de0b667e89df971ad2c8a1d396e11b
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:54:32 +0200

    Task 88: Removed README.md and LICENSE files from packed jar files.

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.33"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.12"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.26"]
-                 [org.clojars.vladimirmarkovic86/common-server "0.3.43"]
-                 [org.clojars.vladimirmarkovic86/sample-middle "0.3.11"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.9"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.10"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.11"]
-                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.34"]
-                 [org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.7"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.34"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.13"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.27"]
+                 [org.clojars.vladimirmarkovic86/common-server "0.3.44"]
+                 [org.clojars.vladimirmarkovic86/sample-middle "0.3.12"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.10"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.11"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.12"]
+                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.35"]
+                 [org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.8"]
                  ]
 
   :min-lein-version "2.0.0"
