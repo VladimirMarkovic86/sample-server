d00ba7d138cb5f2e2c317f4ffe87c2b9c8de1ce9
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 18:01:52 +0200

    Task 88: Rolled back README.md and LICENSE files to appear in jar files.

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.34"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.13"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.27"]
-                 [org.clojars.vladimirmarkovic86/common-server "0.3.44"]
-                 [org.clojars.vladimirmarkovic86/sample-middle "0.3.12"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.10"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.11"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.12"]
-                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.35"]
-                 [org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.8"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.35"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.14"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.28"]
+                 [org.clojars.vladimirmarkovic86/common-server "0.3.45"]
+                 [org.clojars.vladimirmarkovic86/sample-middle "0.3.13"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.11"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.12"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.13"]
+                 [org.clojars.vladimirmarkovic86/audit-lib "0.1.36"]
+                 [org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.9"]
                  ]
 
   :min-lein-version "2.0.0"
