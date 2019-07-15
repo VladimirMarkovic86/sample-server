7cf08edb6dbf21198ac86506f7e083d710cd6a64
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:28:48 +0200

    Task 88: Excluded reports folder from packing in jar file.

--- project.clj
+++ project.clj
@@ -7,13 +7,13 @@
                  [org.clojars.vladimirmarkovic86/server-lib "0.3.33"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.12"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.26"]
-                 [org.clojars.vladimirmarkovic86/common-server "0.3.42"]
+                 [org.clojars.vladimirmarkovic86/common-server "0.3.43"]
                  [org.clojars.vladimirmarkovic86/sample-middle "0.3.11"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.9"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.10"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.11"]
                  [org.clojars.vladimirmarkovic86/audit-lib "0.1.34"]
-                 [org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.6"]
+                 [org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.7"]
                  ]
 
   :min-lein-version "2.0.0"
