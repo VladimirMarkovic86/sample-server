d98b8f1f5b727a9940183d90f7af27808d3ed086
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-09-26 17:43:55 +0200

    Task99: Improved ocr-lib and constrained server-lib to less parallel threads support.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.35"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.36"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.14"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.31"]
                  [org.clojars.vladimirmarkovic86/common-server "0.3.49"]
